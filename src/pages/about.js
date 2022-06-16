4ee7077e8ff1bb7e4051dea46f0270d7ac44ffc8
Hoa Le <hoa.le@synova-solutions.io>
2022-06-16 16:06:45 +0700

    Gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">This is about Page</Layout>
+    )
+}
+
+export default AboutPage;
