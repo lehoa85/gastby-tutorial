9abfed4a272d7c0603049f6b030e3c1e3c5e87ec
Hoa Le <hoa.le@synova-solutions.io>
2022-06-16 17:05:52 +0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import * as React from "react"
 import Layout from '../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home">This is home page</Layout>
+    <Layout pageTitle="Home">
+      <p>This is home page</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      ></StaticImage>
+    </Layout>
   )
 }
 
